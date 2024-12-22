c80fa5d8db4b5450628a5427be1797b47728adf4
fedor <runway067@gmail.com>
2024-12-22 18:06:10 +0400

    first commit

---
+++ crud/demo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet, ModelViewSet
+
+from demo.models import Comment
+from demo.serializers import CommentSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+
+
+
+# Create your views here.
