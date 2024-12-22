0e771a52160ab9f75551721d9fe6f1e6535b32ce
fedor <runway067@gmail.com>
2024-12-22 21:07:24 +0400

    second commit

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'demo',
 ]
@@ -125,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK ={
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',], 'SEARCH_PARAM': 'q',
+}
--- crud/demo/views.py
+++ crud/demo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet, ModelViewSet
 
@@ -6,9 +8,16 @@ from demo.models import Comment
 from demo.serializers import CommentSerializer
 
 
+
+
+
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields =['user',]
+    search_fields = ['text',]
+    ordering_filelds = ['id', 'user', 'text', 'created_at']
 
 
 
